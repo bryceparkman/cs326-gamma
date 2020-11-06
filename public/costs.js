911a0eeaf33071ed860c9c6b471a23ac8fcb9e0d
Bryce Parkman <brycecparkman@gmail.com>
2020-11-06 14:54:12 -0500

    Added dyamic payments section

--- public/costs.js
+++ public/costs.js
@@ -1,5 +1,5 @@
 const totalRent = 660;
-const currentUser = 'Bryce';
+let currentUser = 'Bryce';
 const users = ['Bryce','Hannah','Leon'];
 const data = {
     shares: {
@@ -8,10 +8,7 @@ const data = {
         'Leon': 33.3333
     },
     payments: [
-        {
-            name: 'Bryce',
-            amount: 220
-        }
+
     ]
 };
 
@@ -43,14 +40,45 @@ function calculatePersonalPercent(){
 
 function calculateOverallPercent(user){
     const diff = totalRent - moneySpent(user);
-    console.log(diff)
     if(diff > 0){
         return moneySpent(user) / totalRent;
     }
     return 1;
 }
 
-window.addEventListener('load', () => {
+function htmlToNode(html){ //https://stackoverflow.com/a/35385518
+    const template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
+
+function contributeRent(){
+    const input = document.getElementById('contrinput');
+    let rentValue = parseFloat(input.value);
+    if(!isNaN(rentValue) && input.value >= 0){
+        if(moneySpent(currentUser) + rentValue > (totalRent * data.shares[currentUser] / 100)){
+            rentValue = (totalRent * data.shares[currentUser] / 100) - moneySpent(currentUser);
+        }
+        data.payments.push({
+            name: currentUser,
+            amount: rentValue
+        });
+    }
+
+    if(rentValue > 0){
+        const payments = document.getElementById('payments');
+
+        const htmlString = '<div class="card mb-3 border-0"><div class="row no-gutters"><div class="col-auto"><img src="https://via.placeholder.com/100/9400D3/FFFFFF" class="img-fluid rounded-circle" alt=""> </div><div class="col"><div class="card-block px-4 my-4"> <p class="card-text mb-0">Bryce paid $' + rentValue.toFixed(2) + '</p><p class="card-text percentContributed bryceColor">Contributing ' + ((rentValue / totalRent) * 100).toFixed(2) + '%</p></div></div></div></div>'
+    
+        const node = htmlToNode(htmlString);
+        payments.appendChild(node)
+    }
+    
+    calculatePage();
+}
+
+function calculatePage(){
     const total = document.getElementById('money');
     const owe = document.getElementById('amountToOwe');
     const share = document.getElementById('amountShared');
@@ -60,17 +88,23 @@ window.addEventListener('load', () => {
     share.innerHTML = "out of your " + data.shares[currentUser].toFixed(2) + "% share";
 
     const personalProgress = document.getElementById('personalProgress');
-    personalProgress.style.width = (calculatePersonalPercent() * 100).toFixed(2) + '%';
+    personalProgress.style.width = calculatePersonalPercent() * 100 + '%';
     if(personalProgress.style.width === '100%'){      
-        personalProgress.classList.add('progress-bar-striped')
+        personalProgress.classList.add('progress-bar-striped');
     }
 
     for(const user of users){
         const overallProgress = document.getElementsByClassName('progress' + user)[0];
         overallProgress.style.width = (calculateOverallPercent(user) * 100).toFixed(2) + '%';
-        console.log(overallProgress.style.width)
         if(overallProgress.style.width === data.shares[user].toFixed(2) + '%'){      
-            overallProgress.classList.add('progress-bar-striped')
+            overallProgress.classList.add('progress-bar-striped');
         }
     }
+}
+
+window.addEventListener('load', () => {
+    const contributeButton = document.getElementById('rentButton');
+    contributeButton.addEventListener('click', () => contributeRent());
+
+    calculatePage();
 })
