497e545c5246204900ef9727e1ced24f2e94c1e0
Bryce Parkman <brycecparkman@gmail.com>
2020-11-08 21:58:40 -0500

    Added edit grocery feature

--- public/groceries.js
+++ public/groceries.js
@@ -1,5 +1,6 @@
 const budget = 200;
 let currentUser = 'Bryce';
+let currentElement = null;
 const data = {
     users: [
         {
@@ -93,32 +94,30 @@ function addGrocery() {
     calculatePage();
 }
 
-function editGrocery(element){
-    console.log(element)
+function editItem(isGrocery, element) {
+    const type = (isGrocery ? 'grocery' : 'inventory')
+    const modal = document.getElementById(type + 'ModalEdit');
+    const inputItem = document.getElementById('input' + type + 'ItemEdit');
+    const inputAmount = document.getElementById('input' + type + 'AmountEdit');
+    inputItem.value = element.name;
+    inputAmount.value = element.amount;
+    modal.style.display = 'block';
+    currentElement = element;
 }
 
-function removeGrocery(element){
-    const index = data.groceries.indexOf(element);
-    data.groceries.splice(index,1);
-    getGroceries();
+function removeItem(type, element) {
+    const index = data[type].indexOf(element);
+    data[type].splice(index, 1);
+    getTable(type);
 }
 
-function editInventory(element){
-    console.log(element)
-}
-
-function removeInventory(element){
-    const index = data.inventory.indexOf(element);
-    data.inventory.splice(index,1);
-    getInventory();
-}
-
-function getGroceries(){
-    const paymentsWrapper = document.getElementById('paymentsWrapper');
+function getTable(type) {
+    const id = (type === 'groceries' ? 'paymentsWrapper' : 'inventoryWrapper');
+    const paymentsWrapper = document.getElementById(id);
     paymentsWrapper.innerHTML = '';
     let htmlString = '';
     const nodes = []
-    for (let i = 0; i < data.groceries.length; i++) {
+    for (let i = 0; i < data[type].length; i++) {
         if (i % 2 === 0) {
             if (i > 0) {
                 htmlString += '</div>';
@@ -127,7 +126,7 @@ function getGroceries(){
             }
             htmlString += "<div class='row mx-1'>";
         }
-        htmlString += "<div class='col px-1'><div class='card mb-2'><div class='card-block px-4 my-4'><p class='card-title mb-1'> " + data.groceries[i].name + "</p><p class='card-subtitle text-muted mb-1 fontTwelve'>" + (data.groceries[i].amount !== null ? data.groceries[i].amount : 'Quantity not specified') + "</p><p class='card-subtitle percentContributed " + data.groceries[i].requestedBy.toLowerCase() + "Color'>Requested by " + data.groceries[i].requestedBy + "</p></div><div class='card-footer text-muted'><a href='#_' class='card-link' onclick='editGrocery(data.groceries[" + i + "])'>Edit</a><a href='#_' class='card-link float-right' onclick='removeGrocery(data.groceries[" + i + "])'>Remove</a></div></div></div>";
+        htmlString += "<div class='col px-1'><div class='card mb-2'><div class='card-block px-4 my-4'><p class='card-title mb-1'> " + data[type][i].name + "</p><p class='card-subtitle text-muted mb-1 fontTwelve'>" + (data[type][i].amount !== null ? data[type][i].amount : 'Quantity not specified') + "</p><p class='card-subtitle percentContributed " + data[type][i].requestedBy.toLowerCase() + "Color'>Requested by " + data[type][i].requestedBy + "</p></div><div class='card-footer text-muted'><a href='#_' class='card-link' onclick='editItem(" + (type === 'groceries') + ",data[\"" + type + "\"][" + i + "])'>Edit</a><a href='#_' class='card-link float-right' onclick='removeItem(\"" + type + "\",data[\"" + type + "\"][" + i + "])'>Remove</a></div></div></div>";
     }
     htmlString += '</div>';
     nodes.push(htmlString);
@@ -136,55 +135,42 @@ function getGroceries(){
     }
 }
 
-function getInventory(){
-    const paymentsWrapper = document.getElementById('inventoryWrapper');
-    paymentsWrapper.innerHTML = '';
-    let htmlString = '';
-    const nodes = []
-    for (let i = 0; i < data.inventory.length; i++) {
-        if (i % 2 === 0) {
-            if (i > 0) {
-                htmlString += '</div>';
-                nodes.push(htmlString);
-                htmlString = '';
-            }
-            htmlString += "<div class='row mx-1'>";
-        }
-        htmlString += "<div class='col px-1'><div class='card mb-2'><div class='card-block px-4 my-4'><p class='card-title mb-1'> " + data.inventory[i].name + "</p><p class='card-subtitle text-muted mb-1 fontTwelve'>" + (data.inventory[i].amount !== null ? data.inventory[i].amount : 'Quantity not specified') + "</p><p class='card-subtitle percentContributed " + data.inventory[i].requestedBy.toLowerCase() + "Color'>Bought by " + data.inventory[i].requestedBy + "</p></div><div class='card-footer text-muted'><a href='#_' class='card-link' onclick='editInventory(data.inventory[" + i + "])'>Edit</a><a href='#_' class='card-link float-right' onclick='removeInventory(data.inventory[" + i + "])'>Remove</a></div></div></div>";
-    }
-    htmlString += '</div>';
-    nodes.push(htmlString);
-    for (node of nodes) {
-        paymentsWrapper.appendChild(htmlToNode(node));
-    }
-}
-
-function openModal(type){
-    const modal = document.getElementById(type + 'Modal');
+function openModal(type, isAdd) {
+    const modal = document.getElementById(type + 'Modal' + (isAdd ? '' : 'Edit'));
     modal.style.display = 'block';
 }
 
-function closeModal(type){
-    const modal = document.getElementById(type + 'Modal');
+function closeModal(type, isAdd) {
+    const modal = document.getElementById(type + 'Modal' + (isAdd ? '' : 'Edit'));
     modal.style.display = 'none';
 }
 
-function submitModal(type){
-    const modal = document.getElementById(type + 'Modal');
-    const inputItem = document.getElementById('input' + type + 'Item');
-    const inputAmount = document.getElementById('input' + type + 'Amount');
-    if(inputItem.value.length > 0){
-        console.log(data[type])
-        data[type].push({
-            name: inputItem.value,
-            amount: (inputAmount.value.length > 0 ? inputAmount.value : null),
-            requestedBy: currentUser
-        });
-        modal.style.display = 'none';
-        inputItem.value = ''
-        inputAmount.value = '';
-        getInventory();
-    }    
+function submitModal(type, isAdd) {
+    const modal = document.getElementById(type + 'Modal' + (isAdd ? '' : 'Edit'));
+    const inputItem = document.getElementById('input' + type + 'Item' + (isAdd ? '' : 'Edit'));
+    const inputAmount = document.getElementById('input' + type + 'Amount' + (isAdd ? '' : 'Edit'));
+    if (type === 'grocery') {
+        type = 'groceries';
+    }
+    if (inputItem.value.length > 0) {  
+        if (isAdd) {
+            data[type].push({
+                name: inputItem.value,
+                amount: (inputAmount.value.length > 0 ? inputAmount.value : null),
+                requestedBy: currentUser
+            });
+        }
+        else {
+            const i = data[type].indexOf(currentElement)
+            data[type][i].name = inputItem.value;
+            data[type][i].amount = inputAmount.value;
+            console.log(data[type])
+        }
+    }
+    modal.style.display = 'none';
+    inputItem.value = ''
+    inputAmount.value = '';
+    getTable(type);
 }
 
 function calculatePage() {
@@ -196,8 +182,8 @@ function calculatePage() {
     const width = getWidthString();
     progress.style.width = width;
 
-    getGroceries();
-    getInventory();
+    getTable('groceries');
+    getTable('inventory');
 }
 
 window.addEventListener('load', () => {
@@ -209,7 +195,7 @@ window.addEventListener('load', () => {
 
 window.addEventListener('click', (event) => {
     const modal = document.getElementById('groceryModal');
-    if(event.target === modal) {
+    if (event.target === modal) {
         modal.style.display = 'none';
     }
 })
